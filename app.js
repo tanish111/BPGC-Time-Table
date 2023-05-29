fd8b610dde4e6d8752297ad89990795f006cac58
tanish111 <tanishdesai37@gmail.com>
2023-05-29 19:52:26 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 
 let id = document.getElementById("bits_id")
+let body = document.getElementById("background")
 let button = document.getElementById("Go_Bt")
 let cell = document.getElementsByTagName("td")
+let toogle = document.getElementById("DarkModei")
+let titlebar = document.getElementById("title_b")
+let tab = document.getElementById("main_table")
+let th = document.getElementsByTagName("th")
+let td = document.getElementsByClassName("class_box")
+let title_heading = document.getElementById("title_heading");
+let img_c = document.getElementById('img_change');
 function toTitleCase(str) {
     return str.replace(
       /\w\S*/g,
@@ -39,4 +47,39 @@ function SearchHandler() {
 }).catch(error => console.error(error));
 console.log(tt,id_no)
 }
-button.addEventListener('click',SearchHandler)
+button.addEventListener("click",SearchHandler)
+toogle.addEventListener("change",function(){
+  if(this.checked){
+    id.style.backgroundColor = "white";
+    id.style.color = "black";
+    body.style.backgroundColor = "black";
+    tab.style.border = "2px solid white";
+    Array.from(th).forEach(function (element) {
+      element.style.border = "2px solid white";
+      element.style.color = 'white';
+  });
+  Array.from(td).forEach(function (element) {
+    element.style.backgroundColor = "rgba(146, 154, 171, .3)";
+    element.style.color = 'white';
+});
+title_heading.style.color = 'white';
+img_c.src = 'assets/moon.svg';
+img_c.style.left = '60px';
+  }else {
+    id.style.backgroundColor = "#E6EEFD";
+    id.style.color = "black";
+    body.style.backgroundColor = "white"
+    tab.style.border = "2px solid black";
+    Array.from(th).forEach(function (element) {
+      element.style.border = "2px solid black";
+      element.style.color = 'black';
+  });
+  Array.from(td).forEach(function (element) {
+    element.style.backgroundColor = "rgba(0, 0, 0, .3)";
+    element.style.color = 'black';
+});
+title_heading.style.color = 'black';
+img_c.src = 'assets/sun.svg';
+img_c.style.left = '8px';
+  }
+})
